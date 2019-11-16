4e38c27f8b34281d5e835e93f9781ec68205eb15
aomnchp-s <51848731+aomnchp-s@users.noreply.github.com>
2019-11-16 12:10:19 +0700

    Updates App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>HELLO...I'm AOM...</h1>
+        <h1>HELLO...I'm AOM...EIEI :)</h1>
       </header>
     </div>
   );
