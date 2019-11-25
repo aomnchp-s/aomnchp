af54a4c53665ed053230d7ec05b6955b8b7c6d07
aomnchp-s <51848731+aomnchp-s@users.noreply.github.com>
2019-11-25 22:22:01 +0700

    Update homepage html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HELLO...I'm AOM...EIEI :)</h1>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+        <div></div>
+    )
+  }
 }
 
 export default App;
